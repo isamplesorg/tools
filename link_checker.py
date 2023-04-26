1e90d80b2440ad433a2689195d57536c1736f66f
Danny Mandel <danny.mandel@gmail.com>
2023-04-26 14:13:12 -0700

    Make this actually run concurrently

--- link_checker.py
+++ link_checker.py
@@ -5,7 +5,8 @@ import click
 import httpx
 import logging
 from aiofile import AIOFile, Writer
-from httpx import ReadTimeout, ConnectError, ConnectTimeout, ReadError, TooManyRedirects, RemoteProtocolError
+from httpx import ReadTimeout, ConnectError, ConnectTimeout, ReadError, TooManyRedirects, RemoteProtocolError, \
+    UnsupportedProtocol
 
 
 async def _prepare_url(url: str) -> str:
@@ -15,50 +16,54 @@ async def _prepare_url(url: str) -> str:
         return url
 
 
+async def _check_link(url: str, aiodf: AIOFile, writer: Writer, timeout: int):
+    try:
+        headers = {"User-Agent": "iSamples-Link-Checker/0.0.1"}
+        async with httpx.AsyncClient(follow_redirects=True, timeout=timeout, headers=headers) as client:
+            req = client.build_request("GET", await _prepare_url(url))
+            response = await client.send(req, stream=True)
+            if 200 <= response.status_code < 300:
+                line = f"{url},{response.status_code},SUCCESS"
+            else:
+                text = await response.aread()
+                formatted_text = text.decode("utf-8")[:100].replace("\n", "").replace("\r", "")
+                line = f"{url},{response.status_code},{formatted_text}"
+            await response.aclose()
+    except SSLCertVerificationError as e:
+        line = f"{url},-1,{e.reason}"
+    except ConnectTimeout:
+        line = f"{url},-1,Connect timed out after {timeout} seconds"
+    except ReadTimeout:
+        line = f"{url},-1,Read timed out after {timeout} seconds"
+    except ConnectError as c:
+        line = f"{url},-1,{c.__cause__}"
+    except ValueError as v:
+        line = f"{url},-1,{v.__cause__}"
+    except ReadError as r:
+        line = f"{url},-1,{r.__cause__}"
+    except TooManyRedirects as r:
+        line = f"{url},-1,{r.__cause__}"
+    except RemoteProtocolError as rpe:
+        line = f"{url},-1,{rpe.__cause__}"
+    except SSLError as ssle:
+        line = f"{url},-1,{ssle.__cause__}"
+    except UnsupportedProtocol as unsp:
+        line = f"{url},-1,{unsp.__cause__}"
+    line = line + "\n"
+    await writer(line)
+    await aiodf.fsync()
+
+
 async def _main(output_file: str, conn_count: int, link_set: set[str], timeout: int):
     async with AIOFile(output_file, "w") as aiodf:
         writer = Writer(aiodf)
         header = """link,status,notes\n"""
         await writer(header)
         await aiodf.fsync()
+        tasks = [asyncio.create_task(_check_link(url, aiodf, writer, timeout)) for url in link_set]
+        await asyncio.gather(*tasks)
 
-        count = 0
-        headers = {"User-Agent": "iSamples-Link-Checker/0.0.1"}
-        async with httpx.AsyncClient(limits=httpx.Limits(max_connections=conn_count), follow_redirects=True, timeout=timeout, headers=headers) as client:
-            for url in link_set:
-                count += 1
-                if count % 10 == 0:
-                    logging.debug(f"Processed {count} records")
-                try:
-                    req = client.build_request("GET", await _prepare_url(url))
-                    response = await client.send(req, stream=True)
-                    if 200 <= response.status_code < 300:
-                        line = f"{url},{response.status_code},SUCCESS"
-                    else:
-                        text = await response.aread()
-                        line = f"{url},{response.status_code},{text[:100]}"
-                    await response.aclose()
-                except SSLCertVerificationError as e:
-                    line = f"{url},-1,{e.reason}"
-                except ConnectTimeout:
-                    line = f"{url},-1,Connect timed out after {timeout} seconds"
-                except ReadTimeout:
-                    line = f"{url},-1,Read timed out after {timeout} seconds"
-                except ConnectError as c:
-                    line = f"{url},-1,{c.__cause__}"
-                except ValueError as v:
-                    line = f"{url},-1,{v.__cause__}"
-                except ReadError as r:
-                    line = f"{url},-1,{r.__cause__}"
-                except TooManyRedirects as r:
-                    line = f"{url},-1,{r.__cause__}"
-                except RemoteProtocolError as rpe:
-                    line = f"{url},-1,{rpe.__cause__}"
-                except SSLError as ssle:
-                    line = f"{url},-1,{ssle.__cause__}"
-                await writer(line)
-                await writer("\n")
-                await aiodf.fsync()
+
 
 
 @click.command()
